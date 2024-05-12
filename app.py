3cce5e3757f094f654e8fda1f59ee534c70deef2
Grace <gracefebe611@gmail.com>
2024-05-12 19:40:41 +0800

    First Commit

--- app.py
+++ app.py
@@ -15,10 +15,6 @@ app.config["UPLOAD_FOLDER"] = "./static/profile_pics"
 
 SECRET_KEY = "SPARTA"
 
-MONGODB_CONNECTION_STRING = "mongodb://grace:sparta@ac-luh7xkk-shard-00-00.r4fnst4.mongodb.net:27017,ac-luh7xkk-shard-00-01.r4fnst4.mongodb.net:27017,ac-luh7xkk-shard-00-02.r4fnst4.mongodb.net:27017/?ssl=true&replicaSet=atlas-66k3xp-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(MONGODB_CONNECTION_STRING)
-db = client.db
-
 
 @app.route("/")
 def home():
